9e230e971dae0a7dac029c6267e65f2a3e15156c
troy <troy@43b6720e-02fe-0310-9abc-e7d36aadd2ae>
2005-09-14 18:39:21 +0000

    Branch for rev. 1.0

---
+++ pbstools/trunk/pbstools/web/sql-term.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'DB.php';
+
+echo "<HTML>\n<HEAD>\n<TITLE>\nPHP SQL Terminal\n</TITLE>\n</HEAD>\n<BODY>\n";
+
+echo "<FORM method=\"POST\" action=\"sql-term.php\">\n";
+echo "<TEXTAREA name=\"sql\" cols=\"80\" rows=\"5\">\n";
+if ( isset($_POST['sql']) )
+  {
+    echo $_POST['sql'];
+  }
+echo "</TEXTAREA>\n<BR>\n";
+echo "<INPUT type=\"submit\">\n<INPUT type=\"reset\">\n</FORM>\n";
+
+if ( isset($_POST['sql']) )
+  {
+    $db = DB::connect("mysql://webapp@localhost/pbsacct", FALSE);
+    if ( DB::isError($db) )
+      {
+	die ($db->getMessage());
+      }
+    $result = $db->query($_POST['sql']);
+    if ( DB::isError($db) )
+      {
+	die ($db->getMessage());
+      }
+    else
+      {
+	echo "<TABLE border=1>\n";
+	while ($result->fetchInto($row))
+	  {
+	    echo "<TR>";
+	    $keys=array_keys($row);
+	    foreach ($keys as $key)
+	      {
+		$data=array_shift($row);
+	        echo "<TD>".$data."</TD>";
+	      }
+	    echo "</TR>\n";
+	  }
+	echo "</TABLE>\n";
+      }
+  }
+
+echo "</BODY>\n</HTML>\n";
