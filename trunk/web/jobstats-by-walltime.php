0386b78ef7c0350f8dc2a10340886e6d7f2ab398
troy <troy@43b6720e-02fe-0310-9abc-e7d36aadd2ae>
2007-05-22 19:10:06 +0000

    Changed get_bucketed_metric() to use a CASE statement rather than a UNION SELECT to do bucketing, which is a significant performance improvement.

--- trunk/web/jobstats-by-walltime.php
+++ trunk/web/jobstats-by-walltime.php
@@ -48,6 +48,7 @@ jobstats_input_metric("Job Count vs. Job Length Requested","jobcount_vs_walltime
 jobstats_input_metric("CPU Time vs. Job Length Requested","cpuhours_vs_walltime_req");
 jobstats_input_metric("Queue Time vs. Job Length Requested","qtime_vs_walltime_req");
 jobstats_input_metric("Job Length vs. Job Length Requested","walltime_vs_walltime_req");
+jobstats_input_metric("Expansion Factor vs. Job Length Requested","xfactor_vs_walltime_req");
 jobstats_input_metric("Real Memory vs. Job Length Requested","mem_kb_vs_walltime_req");
 jobstats_input_metric("Virtual Memory vs. Job Length Requested","vmem_kb_vs_walltime_req");
 jobstats_input_metric("Walltime Accuracy vs. Job Length Requested","walltime_acc_vs_walltime_req");
@@ -58,6 +59,8 @@ jobstats_input_spacer();
 jobstats_input_metric("Job Count vs. Job Length","jobcount_vs_walltime");
 jobstats_input_metric("CPU Time vs. Job Length","cpuhours_vs_walltime");
 jobstats_input_metric("Queue Time vs. Job Length","qtime_vs_walltime");
+jobstats_input_metric("Job Length vs. Job Length","walltime_vs_walltime");
+jobstats_input_metric("Expansion Factor vs. Job Length","xfactor_vs_walltime");
 jobstats_input_metric("Real Memory vs. Job Length","mem_kb_vs_walltime");
 jobstats_input_metric("Virtual Memory vs. Job Length","vmem_kb_vs_walltime");
 jobstats_input_metric("Walltime Accuracy vs. Job Length","walltime_acc_vs_walltime");
