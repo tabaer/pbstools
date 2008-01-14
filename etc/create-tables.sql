f89956b349a9b80cc6247c569cad19b03f78d864
Troy Baer <tabaer@users.noreply.github.com>
2008-01-14 14:46:57 +0000

    Added "submit_date", "start_date", and "end_date" columns to DB table, as well as support for populating them in job-db-update.

--- etc/create-tables.sql
+++ etc/create-tables.sql
@@ -12,8 +12,11 @@ CREATE TABLE Jobs (
   nodes        TEXT,
   queue        TINYTEXT,
   submit_ts    INT,
+  submit_date  DATE,
   start_ts     INT,
+  start_date   DATE,
   end_ts       INT,
+  end_date     DATE,
   cput_req     TIME DEFAULT '00:00:00',
   cput         TIME DEFAULT '00:00:00',
   walltime_req TIME DEFAULT '00:00:00',
@@ -31,6 +34,9 @@ CREATE INDEX system_jobs ON Jobs (system);
 CREATE INDEX user_jobs ON Jobs (username);
 CREATE INDEX group_jobs ON Jobs (groupname);
 CREATE INDEX queue_jobs ON Jobs (queue(16));
+CREATE INDEX submit_jobs ON Jobs (submit_date);
+CREATE INDEX start_jobs ON Jobs (start_date);
+CREATE INDEX end_jobs ON Jobs (end_date);
 GRANT INSERT,UPDATE,SELECT ON Jobs TO 'pbsacct'@'localhost' IDENTIFIED BY 'pbsRroxor';
 GRANT SELECT ON Jobs TO 'webapp'@'localhost';
 CREATE TABLE Config (
