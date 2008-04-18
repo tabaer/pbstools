a2ebd2ccd19113d61c5fda713741265a0fc58ede
troy <troy@43b6720e-02fe-0310-9abc-e7d36aadd2ae>
2008-04-18 22:35:16 +0000

    Created GSOC '08 branch.

---
+++ branches/gsoc08/web/active-users.php
@@ -0,0 +1,87 @@
+<?php
+# Copyright 2007, 2008 Ohio Supercomputer Center
+# Revision info:
+# $HeadURL: https://svn.osc.edu/repos/pbstools/trunk/web/active-users.php $
+# $Revision: 199 $
+# $Date: 2007-09-12 13:39:15 -0400 (Wed, 12 Sep 2007) $
+require_once 'dbutils.php';
+require_once 'page-layout.php';
+require_once 'metrics.php';
+
+# accept get queries too for handy command-line usage:  suck all the
+# parameters into _POST.
+if (isset($_GET['system']))
+  {
+    $_POST = $_GET;
+  }
+
+if ( isset($_POST['system']) )
+  { 
+    $title = "Most active users on ".$_POST['system'];
+    if ( isset($_POST['start_date']) && isset($_POST['end_date']) && $_POST['start_date']==$_POST['end_date'] && 
+	     $_POST['start_date']!="" )
+      {
+	$title .= " on ".$_POST['start_date'];
+      }
+    else if ( isset($_POST['start_date']) && isset($_POST['end_date']) && $_POST['start_date']!=$_POST['end_date'] && 
+	      $_POST['start_date']!="" &&  $_POST['end_date']!="" )
+      {
+	$title .= " between ".$_POST['start_date']." and ".$_POST['end_date'];
+      }
+    else if ( isset($_POST['start_date']) && $_POST['start_date']!="" )
+      {
+	$title .= " after ".$_POST['start_date'];
+      }
+    else if ( isset($_POST['end_date']) && $_POST['end_date']!="" )
+      {
+	$title .= " before ".$_POST['end_date'];
+      }
+  }
+else
+  {
+    $title = "Most active users";
+  }
+page_header($title);
+
+$keys = array_keys($_POST);
+if ( isset($_POST['system']) )
+  {
+    $db = db_connect();
+    $sql = "SELECT username, groupname, COUNT(jobid) AS jobcount, SUM(nproc*TIME_TO_SEC(walltime))/3600 AS cpuhrs FROM Jobs WHERE system LIKE '".$_POST['system']."' AND ( ".dateselect("submit",$_POST['start_date'],$_POST['end_date'])." ) GROUP BY username ORDER BY ".$_POST['order']." DESC LIMIT ".$_POST['limit'];
+#    echo "<PRE>".$sql."</PRE>\n";
+    $result = db_query($db,$sql);
+    echo "<TABLE border=\"1\">\n";
+    echo "<TR><TH>user</TH><TH>group</TH><TH>job count</TH><TH>CPU-hours</TH></TR>\n";
+        while ($result->fetchInto($row))
+      {
+	echo "<TR>";
+	$rkeys=array_keys($row);
+	foreach ($rkeys as $key)
+	  {
+	    $data[$key]=array_shift($row);
+	    echo "<TD align=\"right\"><PRE>".htmlspecialchars($data[$key])."</PRE></TD>";
+	  }
+	echo "</TR>\n";
+      }
+    echo "</TABLE>\n";
+  
+    db_disconnect($db);
+    bookmarkable_url();
+  }
+else
+  {
+    begin_form("active-users.php");
+
+    system_chooser();
+    date_fields();
+
+    $choices=array("cpuhrs","jobcount");
+    $defaultchoice="cpuhrs";
+    pulldown("order","Order by",$choices,$defaultchoice);
+    textfield("limit","Max shown","10",4);
+
+    end_form();
+  }
+
+page_footer();
+?>
---
+++ branches/gsoc08/web/jobstats-by-month.php
@@ -0,0 +1,58 @@
+<?php
+# Copyright 2006 Ohio Supercomputer Center
+# Revision info:
+# $HeadURL: https://svn.osc.edu/repos/pbstools/trunk/web/jobstats.php $
+# $Revision: 93 $
+# $Date: 2006-02-15 13:53:25 -0500 (Wed, 15 Feb 2006) $
+require_once 'page-layout.php';
+require_once 'dbutils.php';
+require_once 'metrics.php';
+require_once 'site-specific.php';
+
+$title = "Job statistics";
+if ( isset($_POST['system']) )
+  {
+    $title .= " for ".$_POST['system'];
+    if ( isset($_POST['start_date']) && isset($_POST['end_date']) &&
+	 $_POST['start_date']==$_POST['end_date'] && 
+	 $_POST['start_date']!="" )
+      {
+	$title .= " on ".$_POST['start_date'];
+      }
+    else if ( isset($_POST['start_date']) && isset($_POST['end_date']) && $_POST['start_date']!=$_POST['end_date'] && 
+	      $_POST['start_date']!="" &&  $_POST['end_date']!="" )
+      {
+	$title .= " from ".$_POST['start_date']." to ".$_POST['end_date'];
+      }
+    else if ( isset($_POST['start_date']) && $_POST['start_date']!="" )
+      {
+	$title .= " after ".$_POST['start_date'];
+      }
+    else if ( isset($_POST['end_date']) && $_POST['end_date']!="" )
+      {
+	$title .= " before ".$_POST['end_date'];
+      }
+  }
+page_header($title);
+
+begin_form("jobstats.php");
+
+virtual_system_chooser();
+date_fields();
+
+// by month
+jobstats_input_header();
+jobstats_input_metric("Job Count by Month","jobcount_vs_month");
+jobstats_input_metric("CPU Time by Month","cpuhours_vs_month");
+jobstats_input_metric("Job Length by Month","walltime_vs_month");
+jobstats_input_metric("Queue Time by Month","qtime_vs_month");
+jobstats_input_metric("Backlog by Month","backlog_vs_month");
+jobstats_input_metric("Expansion Factor by Month","xfactor_vs_month");
+jobstats_input_metric("Active Users by Month","users_vs_month");
+jobstats_input_metric("Active Groups/Projects by Month","groups_vs_month");
+jobstats_input_footer();
+
+end_form();
+
+page_footer();
+?>
