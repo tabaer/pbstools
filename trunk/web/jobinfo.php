631777007ad9dc4eef1e9e66e31d960fd3979dd6
troy <troy@43b6720e-02fe-0310-9abc-e7d36aadd2ae>
2008-05-07 21:22:38 +0000

    Added "software" column to all job info reports.

--- trunk/web/jobinfo.php
+++ trunk/web/jobinfo.php
@@ -27,7 +27,7 @@ page_header($title);
 $props=array("username","groupname","jobname","nproc","mppe","mppssp",
 	     "nodes","queue","submit_ts","start_ts","end_ts","cput_req",
 	     "cput","walltime_req","walltime","mem_req","mem_kb",
-	     "vmem_req","vmem_kb","hostlist","exit_status","script");
+	     "vmem_req","vmem_kb","software","hostlist","exit_status","script");
 
 // special key "all=1" turns on all the $props.
 if (!empty($_POST['all'])) {
