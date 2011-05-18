fbd1f2e4ea9a156d2ba5a037f2efbad3952baea8
troy <troy@43b6720e-02fe-0310-9abc-e7d36aadd2ae>
2011-05-18 16:06:25 +0000

    Create branch for Nicholas Lineback to work in.

---
+++ branches/lineback-Su2011/web/jobs-by-account.php
@@ -0,0 +1,121 @@
+<?php
+# Copyright 2006, 2007, 2008 Ohio Supercomputer Center
+# Copyright 2008, 2009, 2010 University of Tennessee
+# Revision info:
+# $HeadURL$
+# $Revision$
+# $Date$
+require_once 'dbutils.php';
+require_once 'page-layout.php';
+require_once 'metrics.php';
+
+# accept get queries too for handy command-line usage:  suck all the
+# parameters into _POST.
+if (isset($_GET['account']))
+  {
+    $_POST = $_GET;
+  }
+
+if ( isset($_POST['account']) )
+  { 
+    $title = "Jobs owned by account ".$_POST['account']." on ".$_POST['system'];
+    if ( isset($_POST['start_date']) && isset($_POST['end_date']) && $_POST['start_date']==$_POST['end_date'] && 
+	     $_POST['start_date']!="" )
+      {
+	$title .= " submitted on ".$_POST['start_date'];
+      }
+    else if ( isset($_POST['start_date']) && isset($_POST['end_date']) && $_POST['start_date']!=$_POST['end_date'] && 
+	      $_POST['start_date']!="" &&  $_POST['end_date']!="" )
+      {
+	$title .= " submitted between ".$_POST['start_date']." and ".$_POST['end_date'];
+      }
+    else if ( isset($_POST['start_date']) && $_POST['start_date']!="" )
+      {
+	$title .= " submitted after ".$_POST['start_date'];
+      }
+    else if ( isset($_POST['end_date']) && $_POST['end_date']!="" )
+      {
+	$title .= " submitted before ".$_POST['end_date'];
+      }
+  }
+else
+  {
+    $title = "Jobs by account";
+  }
+page_header($title);
+
+$keys = array_keys($_POST);
+if ( isset($_POST['account']) )
+  {
+    $db = db_connect();
+    $sql = "SELECT jobid,account,username";
+    foreach ($keys as $key)
+      {
+	if ( isset($_POST[$key]) && $key!='jobid' && $key!='account' && $key!='username' &&
+	     $key!='start_date' && $key!='end_date' )
+	  {
+	    $sql .= ",".$key;
+	  }
+      }
+    $sql = $sql." FROM Jobs WHERE account = '".$_POST['account']."' AND system LIKE '".$_POST['system']."' AND ( ".dateselect("submit",$_POST['start_date'],$_POST['end_date'])." ) ORDER BY submit_ts;";
+#    echo "<PRE>".$sql."</PRE>\n";
+    $result = db_query($db,$sql);
+    echo "<TABLE border=\"1\">\n";
+    $col[0]="jobid";
+    $col[1]="username";
+    $col[2]="account";
+    $ncols=3;
+    echo "<TR><TH>jobid</TH><TH>account</TH><TH>username</TH>";
+    foreach ($keys as $key)
+      {
+	if ( $key!='jobid' && $key!='username' && $key!='account' && $key!='start_date' && $key!='end_date' )
+	  {
+	    echo "<TH>".$key."</TH>";
+	    $col[$ncols]=$key;
+	    $ncols++;
+	  }
+      }
+    echo "</TR>\n";
+    
+    while ($result->fetchInto($row))
+      {
+	$rkeys=array_keys($row);
+	echo "<TR>";
+	foreach ($rkeys as $key)
+	  {
+	    $data[$key]=array_shift($row);
+	    if ( $col[$key]=="submit_ts" || $col[$key]=="start_ts" || $col[$key]=="end_ts")
+	      {
+		echo "<TD><PRE>".date("Y-m-d H:i:s",$data[$key])."</PRE></TD>\n";
+	      }
+	    else
+	      {
+		echo "<TD><PRE>".htmlspecialchars($data[$key])."</PRE></TD>";
+	      }
+	  }
+	echo "</TR>\n";
+      }
+    echo "</TABLE>\n";
+
+    db_disconnect($db);
+    bookmarkable_url();
+  }
+else
+  {
+    begin_form("jobs-by-account.php");
+
+    text_field("Account","account",16);
+    system_chooser();
+    date_fields();
+
+    $props=array("jobname","nproc","mppe","mppssp",
+		 "nodes","feature","gres","queue","qos","submit_ts","start_ts","end_ts","cput_req",
+		 "cput","walltime_req","walltime","mem_req","mem_kb",
+		 "vmem_req","vmem_kb","software","submithost","hostlist","exit_status","script");
+    checkboxes_from_array("Properties",$props);
+
+    end_form();
+  }
+
+page_footer();
+?>
