36c734dcc6ce1b81aab4f66884d9339e954babab
Troy Baer <tabaer@users.noreply.github.com>
2014-07-21 20:32:08 +0000

    Add page generation timing to web report pages.

--- web/active-groups.php
+++ web/active-groups.php
@@ -1,6 +1,6 @@
 <?php
 # Copyright 2007, 2008 Ohio Supercomputer Center
-# Copyright 2009, 2011 University of Tennessee
+# Copyright 2009, 2011, 2014 University of Tennessee
 # Revision info:
 # $HeadURL$
 # $Revision$
@@ -71,6 +71,7 @@ if ( isset($_POST['system']) )
     echo "</TABLE>\n";
   
     db_disconnect($db);
+    page_timer();
     bookmarkable_url();
   }
 else
--- web/usage-summary.php
+++ web/usage-summary.php
@@ -1,6 +1,6 @@
 <?php
 # Copyright 2007, 2008 Ohio Supercomputer Center
-# Copyright 2008, 2009, 2010, 2011 University of Tennessee
+# Copyright 2008, 2009, 2010, 2011, 2014 University of Tennessee
 # Revision info:
 # $HeadURL$
 # $Revision$
@@ -310,6 +310,7 @@ if ( isset($_POST['system']) )
       }
 
     db_disconnect($db);
+    page_timer();
     bookmarkable_url();
   }
 else
