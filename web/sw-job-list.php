69caae2e9005c2524bafaf608740c96296d61156
Troy Baer <tabaer@users.noreply.github.com>
2016-03-23 15:13:48 +0000

    Update some reports that didn't get the datelogic changes to bounded_walltime() and friends.

--- web/sw-job-list.php
+++ web/sw-job-list.php
@@ -58,7 +58,7 @@ $keys = array_keys($_POST);
 if ( isset($_POST['system']) )
   {
     #$pkgmatch = software_match_list();
-    $sql = "SELECT system, jobid, username, groupname, account, jobname, nproc, nodes, mem_req, mem_kb, FROM_UNIXTIME(submit_ts), FROM_UNIXTIME(start_ts), FROM_UNIXTIME(end_ts), walltime_req, walltime, ".cpuhours($db,$_POST['system'],$_POST['start_date'],$_POST['end_date'])." AS cpuhours, ".charges($db,$_POST['system'],$_POST['start_date'],$_POST['end_date'])." AS charges, queue, script FROM Jobs WHERE ( ".sysselect($_POST['system'])." ) AND ( ".dateselect($_POST['datelogic'],$_POST['start_date'],$_POST['end_date'])." ) AND sw_app='".$_POST['pkg']."' ORDER BY start_ts;";
+    $sql = "SELECT system, jobid, username, groupname, account, jobname, nproc, nodes, mem_req, mem_kb, FROM_UNIXTIME(submit_ts), FROM_UNIXTIME(start_ts), FROM_UNIXTIME(end_ts), walltime_req, walltime, ".cpuhours($db,$_POST['system'],$_POST['start_date'],$_POST['end_date'],$_POST['datelogic'])." AS cpuhours, ".charges($db,$_POST['system'],$_POST['start_date'],$_POST['end_date'],$_POST['datelogic'])." AS charges, queue, script FROM Jobs WHERE ( ".sysselect($_POST['system'])." ) AND ( ".dateselect($_POST['datelogic'],$_POST['start_date'],$_POST['end_date'])." ) AND sw_app='".$_POST['pkg']."' ORDER BY start_ts;";
     #echo "<PRE>".$sql."</PRE>\n";
     $columns = array("system", "jobid", "username", "groupname", "account", "jobname", "nproc", "nodes", "mem_req", "mem_used", "submit_time", "start_time", "end_time", "walltime_req", "walltime", "cpuhours", "charges", "queue", "script");
     $file_base = $_POST['system']."-joblist-".$_POST['start_date']."-".$_POST['end_date'];
